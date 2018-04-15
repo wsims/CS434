e48f5d80526f39c3fc5c1c34869b71c6ca05aa76
wsims <willy.simz@gmail.com>
2018-04-14 21:47:53 -0700

    adds plotting for random features

--- src/lin_reg/part1.py
+++ src/lin_reg/part1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def get_housing_data(file, d):
     y_list = []
@@ -166,5 +167,24 @@ def main():
     ASE_test = get_ASE_rand('housing_test.txt', w, x_test, y_test)
     print("Training data ASE: %f" % ASE_train)
     print("Testing data ASE: %f" % ASE_test)
-    
+
+    # Plot with respect to D
+    rand_ASE_train = []
+    rand_ASE_test = []
+    for i in range(0, 200, 2):
+        x, y = get_housing_data('housing_train.txt', i)
+        w = np.linalg.inv(x.T*x)*x.T*y
+        x_test, y_test = get_housing_data('housing_test.txt', i)
+        rand_ASE_train.append(get_ASE_rand('housing_train.txt', w, x, y))
+        rand_ASE_test.append(get_ASE_rand('housing_test.txt', w, x_test, y_test))
+
+    runs = range(0, 200, 2)
+    plt.plot(runs, rand_ASE_train, '-b', label='training data')
+    plt.plot(runs, rand_ASE_test, '-r', label='testing data')
+    plt.legend(loc='lower right')
+    plt.xlabel('D')
+    plt.ylabel('Average Standard Error')
+    plt.title('Training and Testing ASEs as a function of D')
+    plt.show()
+
 main()
