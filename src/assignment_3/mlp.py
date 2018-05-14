04eb4a79a4ec9f3a9411b6fb14140effffef4211
noblek <noblek@oregonstate.edu>
2018-05-13 21:15:54 -0700

    Completed first working interation of mlp

--- src/assignment_3/mlp.py
+++ src/assignment_3/mlp.py
@@ -1,9 +1,122 @@
-import cPickle
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cPickle
+
+cuda = torch.cuda.is_available()
+print('Using PyTorch version:', torch.__version__, 'CUDA:', cuda)
+
+#torch.manual_seed(42)
+#if cuda:
+#    torch.cuda.manual_seed(42)
 
 def unpickle(file):
     with open(file, 'rb') as fo:
         dict = cPickle.load(fo)
     return dict
 
+def get_normalized_data(file):
+    dict = unpickle(file)
+    print dict['data']
+    dict['data'] = dict['data'].astype(float)
+    dict['data'] = dict['data']/255
+    print dict['data']
+    return dict
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(32*32*3, 100)
+        self.fc1_drop = nn.Dropout(0.2)
+        self.fc2 = nn.Linear(100, 100)
+        self.fc2_drop = nn.Dropout(0.2)
+        self.fc3 = nn.Linear(100, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 32*32*3)
+        x = F.sigmoid(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = F.sigmoid(self.fc2(x))
+        x = self.fc2_drop(x)
+        return F.log_softmax(self.fc3(x), 0)
+
+def train(epoch, log_interval=100):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def validate(loss_vector, accuracy_vector):
+    model.eval()
+    val_loss, correct = 0, 0
+    for data, target in validation_loader:
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        val_loss += F.nll_loss(output, target).item()
+        pred = output.data.max(1)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data).cpu().sum()
+
+    val_loss /= len(validation_loader)
+    loss_vector.append(val_loss)
+
+    accuracy = 100. * correct / len(validation_loader.dataset)
+    accuracy_vector.append(accuracy)
+    
+    print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        val_loss, correct, len(validation_loader.dataset), accuracy))
+
+batch_size = 32
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.CIFAR10('.', train=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor()
+                   ])),
+    batch_size=batch_size, shuffle=True, **kwargs)
+
+validation_loader = torch.utils.data.DataLoader(
+    datasets.CIFAR10('.', train=False, transform=transforms.Compose([
+                       transforms.ToTensor()
+                   ])),
+    batch_size=batch_size, shuffle=False, **kwargs)
+
 if __name__ == '__main__':
+    #get_normalized_data('cifar-10-batches-py/data_batch_1')
+
+    model = Net()
+    if cuda:
+        model.cuda()
+
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.5)
+
+    print(model)
+
+    #print train_loader.X_train
+
+    epochs = 10
+    
+    lossv, accv = [], []
+    for epoch in range(1, epochs + 1):
+        train(epoch)
+        validate(lossv, accv)
