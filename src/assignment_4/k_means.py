b1920a9e3bec9555a597ad6f35c41f2e94e8d618
noblek <noblek@oregonstate.edu>
2018-05-26 19:13:31 -0700

    Added docstrings for assignment 4

--- src/assignment_4/k_means.py
+++ src/assignment_4/k_means.py
@@ -1,46 +1,88 @@
+"""
+K-means Algorithm Implementation
+
+Performs K-means clustering on a set of 28x28 images of handwritten
+digits.  For k=2, this script plots the SSE versus the number of
+clustering iterations and stores the plot as 'k_means.png'.  This 
+script also plots the lowest convergence SSE out of 10 trials for
+k = 2, 3, ..., 10 and stores the plot as 'k_clusters_2_2.png'.
+
+This script is used to satisfy part 2 in its entirety for assignment 4.
+
+Usage:
+    $ python k_means.py
+
+"""
+
 import numpy as np
 import random
 import math
 import matplotlib.pyplot as plt
 
 class Cluster(object):
+    """Individual Cluster object"""
+
     def __init__(self, mean):
         self.mean = mean
         self.cluster = []
 
     def add_data(self, obs):
+        """Add a data point to the cluster"""
         self.cluster.append(obs)
 
-    def distance(self, obs):
-        x = obs - self.mean
+    def distance(self, obs, mean=None):
+        """Calculate the distance from an observation to the mean value
+        of a cluster.
+
+        Inputs:
+            obs (numpy column vector): A single data point
+            mean (numpy column vector): optional value; uses the 
+                cluster mean value if not provided.
+
+        Outputs:
+            distance (float): The euclidean distance squared from the
+                observation to the mean.
+        """
+        if mean == None:
+            mean = self.mean
+        x = obs - mean
         distance = np.dot(x.T, x).item(0)
 
         return distance
 
     def get_SSE(self):
+        """Returns the SSE for a cluster using the mean of the points
+        currently in the cluster.
+
+        """
         SSE = 0
+        mean = self.get_new_mean()
         for value in self.cluster:
-            SSE += self.distance(value)
+            SSE += self.distance(value, mean)
         return SSE
 
     def get_new_mean(self):
+        """Calculates the mean of all observations in the cluster"""
         new_mean = []
         if len(self.cluster) == 0:
-            new_mean = self.mean
+            new_mean = self.mean # Necessary to deal with emtpy clusters
         else:
-            for i in range(len(self.mean)):
+            for i in range(len(self.mean)): # iterate over each feature
                 sum = 0
-                for value in self.cluster:
+                for value in self.cluster: # iterate over each data point
                     sum += value[i]
                 sum /= float(len(self.cluster))
                 new_mean.append(sum)
         return new_mean
 
 class KCluster(object):
+    """Full KCluster object"""
+
     def __init__(self, means):
         self.clusters = [Cluster(means[i]) for i in range(len(means))]
 
     def _get_closest_cluster(self, obs):
+        """Returns the index of the closest cluster to an observation"""
         min_distance = float("inf")
         min_index = None
         for i, cluster in enumerate(self.clusters):
@@ -48,21 +90,29 @@ class KCluster(object):
             if distance < min_distance:
                 min_distance = distance
                 min_index = i
-    
+
         return min_index
-    
+
     def cluster_data(self, data):
+        """Sort data set into clusters.
+
+        Inputs:
+            data (2D numpy matrix): Each row is a data point.
+
+        """
         for row in data:
             index = self._get_closest_cluster(row)
             self.clusters[index].add_data(row)
 
     def get_SSE(self):
+        """Calculate the sum of the SSE for all clusters"""
         SSE = 0
         for clust in self.clusters:
             SSE += clust.get_SSE()
         return SSE
 
     def get_new_means(self):
+        """Returns a list of new means for each cluster"""
         means = []
         for value in self.clusters:
             mean = value.get_new_mean()
@@ -70,6 +120,9 @@ class KCluster(object):
         return means
 
 def get_data(file='data-1.txt'):
+    """Extract data from a file and return it
+    in the form of a numpy matrix.
+    """
     f = open(file, 'r')
     data_list = []
 
@@ -80,6 +133,17 @@ def get_data(file='data-1.txt'):
     return data_list
 
 def get_k_seeds(data, k):
+    """Randomly selects k data points from a data set to use as initial
+    means.
+
+    Inputs:
+        data (2D numpy matrix): each row is a data point
+        k (int): number of points to select
+
+    Outputs:
+        means (list of numpy vectors): List of randomly selected intial means.
+
+    """
     means = []
     length = len(data)
     for i in range(k):
@@ -87,32 +151,24 @@ def get_k_seeds(data, k):
         means.append(data[seed_index])
     return means
 
-def compare(old_means, new_means):
-    comp = True
-    for i in range(len(old_means)):
-        if not np.array_equal(old_means[i], new_means[i]):
-            comp = False
-            break
-    return comp
-
 def k_means(data, k):
+    """Perform k_means clustering on a data set"""
     # Pick seeds
-    new_means = get_k_seeds(data, k)
+    means = get_k_seeds(data, k)
 
     # Create initial cluster
-    cluster_set = KCluster(new_means)
+    cluster_set = KCluster(means)
     cluster_set.cluster_data(data)
-    SSE_list = [cluster_set.get_SSE()]
-    old_means = new_means
-    new_means = cluster_set.get_new_means()
+    SSE_list = [-1, cluster_set.get_SSE()]
 
     # Create new clusters until means converge
-    while not compare(old_means, new_means):
-        cluster_set = KCluster(new_means)
+    while abs(SSE_list[-2] - SSE_list[-1]) > 1E-10:
+        means = cluster_set.get_new_means()
+        cluster_set = KCluster(means)
         cluster_set.cluster_data(data)
         SSE_list.append(cluster_set.get_SSE())
-        old_means = new_means
-        new_means = cluster_set.get_new_means()
+
+    SSE_list.pop(0)
     return SSE_list
 
 if __name__ == '__main__':
@@ -128,7 +184,7 @@ if __name__ == '__main__':
     # Question 2.1 plot
     plt.figure(1)
     plt.plot(cluster_updates, SSE_list, '-b')
-    plt.xlabel('Number of cluster updates')
+    plt.xlabel('Number of clustering iterations')
     plt.ylabel('SSE')
     plt.title('K-means algorithm SSE convergence with k=2')
     plt.savefig("k2_means.png")
@@ -142,7 +198,7 @@ if __name__ == '__main__':
         for j in range(10):
             print("Performing trial %d..." % (j+1))
             SSE_converge_list = k_means(data, i)
-            SSE = SSE_converge_list[len(SSE_converge_list)-1]
+            SSE = SSE_converge_list[-1]
             if SSE < SSE_min:
                 SSE_min = SSE
         SSE_list.append(SSE_min)
--- src/assignment_4/pca.py
+++ src/assignment_4/pca.py
@@ -1,7 +1,28 @@
+"""
+Principal Component Analysis Implementation
+
+Uses Principal Component Analysis to perform dimension
+reduction on a set of 28x28 images to reduce the dimensionality
+from 784 features down to 10.  The mean image of the data set
+is stored as 'mean.png'.  The 10 eigenvectors produced are
+stored as 'eigenvector{n}.png' for n = 1, 2,..., 10.  The images
+that have the largest value in each dimension are stored as
+'dimension{n}.png'
+
+This script is used to satisfy part 3 in its entirety for assignment 4.
+
+Usage:
+    $ python pca.py
+
+"""
+
 import numpy as np
 from PIL import Image
 
 def get_data(file='data-1.txt'):
+    """Extract data from a file and return it
+    in the form of a numpy matrix.
+    """
     f = open(file, 'r')
     data_list = []
 
@@ -13,11 +34,30 @@ def get_data(file='data-1.txt'):
     return data_mat
 
 def get_mean(data):
+    """Returns the mean of the rows in the data set.
+
+    Inputs:
+        data (2D numpy matrix): each row is a data point
+
+    Outputs:
+        mean (numpy column vector): mean of all data points
+
+    """
     mean = np.sum(data, axis=0)
     mean = mean / len(data)
     return np.matrix(mean).T
 
 def get_covariance(data, mean):
+    """Returns the covariance matrix for the data set.
+
+    Inputs:
+        data (2D numpy matrix): each row is a data point
+        mean (numpy column vector): mean of all data points
+
+    Outputs:
+        cov_mat (2D numpy matrix): Covariance matrix for the data set
+
+    """
     n, d = data.shape
     cov_mat = np.zeros((d, d))
     for row in data:
@@ -29,6 +69,19 @@ def get_covariance(data, mean):
     return cov_mat
 
 def get_eigen(cov_mat):
+    """Returns a sorted list of associated eigenvalues and eigenvectors
+    for a given covariance matrix.
+
+    Inputs:
+        cov_mat (2D numpy matrix): Covariance matrix for the data set
+
+    Outputs:
+        eig_list (list): Sorted list of eigen values and eigen vectors.
+            Eigenvalues occupy the first entry in each sublist, while
+            Eigenvectors occupy the second.  Eigenvectors are in the form
+            of a numpy column vector.
+
+    """
     eigenvalues, eigenvectors = np.linalg.eig(cov_mat)
     eig_list = []
     for i, eig_val in enumerate(eigenvalues):
@@ -38,6 +91,13 @@ def get_eigen(cov_mat):
     return eig_list
 
 def save_image(vector, file):
+    """Translates a 784x1 numpy matrix into a scaled image and saves it.
+
+    Inputs:
+        vector (numpy column vector): The data we wish to save as an image.
+        file (str): File name we wish to save the image as.
+
+    """
     vector_temp = vector.real # get only the real components from each entry
     max = vector_temp.max()
     vector_temp = vector_temp * (255.0 / max)
@@ -48,6 +108,22 @@ def save_image(vector, file):
     print "Image saved as '" + file + "'"
 
 def get_projection_mag(image, eigenvector, mean):
+    """Returns the magnitude of the projection of an image onto
+    an eigenvector.
+
+    Inputs:
+        image (numpy column vector): The data point we wish to project
+            onto the eigenvector.
+        eigenvector (numpy column vector): The eigenvector we will project
+            onto.
+        mean (numpy column vector): The mean of the data set.  This is used
+            to translate and normalize image vectors.
+
+    Outputs:
+        magnitude (float): The magnitude of the projection of the image
+            onto the eigenvector.
+
+    """
     translation = image - mean
     magnitude = np.inner(translation.T, eigenvector.T).item(0)
     return magnitude
