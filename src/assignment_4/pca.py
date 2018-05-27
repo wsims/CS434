0da0a86cf906619cadcd215a6f7f3cb7adcfaea6
noblek <noblek@oregonstate.edu>
2018-05-26 17:50:55 -0700

    Added projection code for part 3.3

--- src/assignment_4/pca.py
+++ src/assignment_4/pca.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image
 
 def get_data(file='data-1.txt'):
     f = open(file, 'r')
@@ -36,11 +37,47 @@ def get_eigen(cov_mat):
     eig_list.reverse()
     return eig_list
 
+def save_image(vector, file):
+    vector_temp = vector.real # get only the real components from each entry
+    max = vector_temp.max()
+    vector_temp = vector_temp * (255.0 / max)
+    im = Image.fromarray(vector_temp.reshape(28, 28))
+    if im.mode != 'RGB':
+        im = im.convert('RGB')
+    im.save(file)
+    print "Image saved as '" + file + "'"
+
+def get_projection_mag(image, eigenvector, mean):
+    translation = image - mean
+    magnitude = np.inner(translation.T, eigenvector.T).item(0)
+    return magnitude
+
 if __name__ == '__main__':
     data = get_data()
     mean = get_mean(data)
     cov_mat = get_covariance(data, mean)
     eig_list = get_eigen(cov_mat)
+
+    # Question 3.1
     for i in range(10):
-        #print(eig_list[i][0])
         print("Eigenvalue %d: %f" % (i+1, eig_list[i][0].real))
+
+    # Question 3.2
+    save_image(mean, "mean.png")
+
+    for i in range(10):
+        save_image(eig_list[i][1], "eigenvector" + str(i+1) + ".png")
+
+    # Question 3.3
+    image_list = [[-float('inf'), None] for i in range(10)]
+    for row in data:
+        for i in range(10):
+            projection_mag = get_projection_mag(row.T, eig_list[i][1].real, mean)
+            if projection_mag > image_list[i][0]:
+                image_list[i][0] = projection_mag
+                image_list[i][1] = row
+
+    for i, image in enumerate(image_list):
+        save_image(image[1].T, "dimension" + str(i+1) + ".png")
+
+
